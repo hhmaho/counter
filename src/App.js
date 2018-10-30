c992c0017d7d77b57b0fe6de84a4787a5e825111
hhmaho <42540357+hhmaho@users.noreply.github.com>
2018-10-30 10:03:25 +0100

    buttons

--- src/App.js
+++ src/App.js
@@ -2,11 +2,27 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { number: 0 }
+  }
   render() {
     return (
       <div>
-        Hello World
-      </div>
+        <button onClick={() => {
+          this.setState(
+            { number: this.state.number + 1 }
+          )
+        }
+        }>+</button>
+        {console.log(this.state.number)}
+        <button onClick={() => {
+          this.setState({
+            number: this.state.number - 1
+          })
+        }}>-</button>
+        <p>{this.state.number}</p>
+      </div >
     );
   }
 }
